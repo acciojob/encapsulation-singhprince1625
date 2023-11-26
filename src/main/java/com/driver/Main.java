23e5e032d5584bdc52e4abb028be8fb4ecdd40b5
singhprince1625 <singhprince73700@gmail.com>
2023-11-26 22:31:31 +0530

    1st commit on 26/11/2023

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+
+    // obj.member = "Mohit Raj";
+    // System.out.println(obj.member);
+
+    obj.setMember("Mohit Raj");
+    System.out.println(obj.getMember());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String member;
+
+    public String getMember(){
+        return this.member;
+    }
+
+    public void setMember(String member){
+        this.member= member;
+    }
 }
